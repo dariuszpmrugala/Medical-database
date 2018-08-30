ce8f5c92c606ed74f66ce8140f22f1bcc5f5f21e
Reese Grimsley <reesul@tamu.edu>
2018-08-30 11:35:08 -0500

    Pulled Jonathon's files into respective packages

---
+++ app/src/main/java/edu/tamu/ecen/capstone/patientmd/database/DatabaseEntry.java
@@ -0,0 +1,74 @@
+package edu.tamu.ecen.capstone.patientmd.database;
+
+/**
+ * Created by Jonathan on 3/31/2018.
+ */
+
+public class DatabaseEntry {
+    private String id;
+    private String date;
+    private String tests;
+    private String result;
+    private String units;
+    private String reference_interval;
+
+    @Override
+    public String toString() {
+        return "DatabaseEntry{" +
+                "id='" + id + '\'' +
+                ", date='" + date + '\'' +
+                ", tests='" + tests + '\'' +
+                ", result='" + result + '\'' +
+                ", units='" + units + '\'' +
+                ", reference_interval='" + reference_interval + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTests() {
+        return tests;
+    }
+
+    public void setTests(String tests) {
+        this.tests = tests;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    public String getReference_interval() {
+        return reference_interval;
+    }
+
+    public void setReference_interval(String reference_interval) {
+        this.reference_interval = reference_interval;
+    }
+}
